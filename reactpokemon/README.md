db375fdcd917c45e150c704168535d8ea93c595c
semacitrik <semahrmnc.de@gmail.com>
2024-08-17 17:27:34 +0300

    first

---
+++ reactpokemon/README.md
@@ -0,0 +1,5 @@
+<h1>Pokemon Game</h1>
+
+# Coded with React.
+
+<img src="../reactpokemon/public/Zight Recording 2024-08-17 at 05.23.28 PM.gif"/>
---
+++ reactpokemon/src/App.js
@@ -0,0 +1,15 @@
+
+import "./Pokedex.css";
+import Pokegame from "./Pokegame";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1 className='Pokedex-h1'>Pokemon Go Card Game</h1>
+      <Pokegame/>
+      
+    </div>
+  )
+}
+
+export default App
